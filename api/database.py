a06a001dc5017d1ba613d09b9c51e68c8caac18e
Devin Lim <davay@devinl.im>
2025-03-25 06:14:37 -0700

    feat: updated seeding to match new models

--- api/database.py
+++ api/database.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session, SQLModel, create_engine, inspect
 
-from models import Company, Job
+from models import Category, Company, Job
 
 DATABASE_URL = "postgresql://postgres:example@localhost:5432/postgres"
 engine = create_engine(DATABASE_URL)
@@ -8,7 +8,7 @@ engine = create_engine(DATABASE_URL)
 
 def get_engine():
     inspector = inspect(engine)
-    models = [Company, Job]
+    models = [Category, Company, Job]
     tables = [model.__tablename__ for model in models]
     missing_tables = [table for table in tables if not inspector.has_table(table)]
 
@@ -22,21 +22,32 @@ def get_engine():
 def seed_database(engine):
     print("Seeding the database...")
     with Session(engine) as session:
-        companies = [
-            Company(
-                name="Amazon",
+        companies = {
+            "amazon": Company(name="Amazon"),
+            "microsoft": Company(name="Microsoft"),
+            "apple": Company(name="Apple"),
+        }
+        session.add_all(companies.values())
+        session.commit()
+
+        categories = {
+            "amazon_general": Category(
+                company_id=companies["amazon"].id,
+                name="General",
                 url="https://www.amazon.jobs/en/search?offset=0&result_limit=10&sort=recent&country%5B%5D=USA",
             ),
-            Company(
-                name="Microsoft",
+            "microsoft_general": Category(
+                company_id=companies["microsoft"].id,
+                name="General",
                 url="https://jobs.careers.microsoft.com/global/en/search?lc=United%20States&l=en_us&pg=1&pgSz=20&o=Relevance&flt=true&ulcs=true&cc=United%20States&ref=cms",
             ),
-            Company(
-                name="Apple",
+            "apple_general": Category(
+                company_id=companies["apple"].id,
+                name="General",
                 url="https://jobs.apple.com/en-us/search?location=united-states-USA",
             ),
-        ]
-        session.add_all(companies)
+        }
+        session.add_all(categories.values())
         session.commit()
 
 
