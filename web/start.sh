f9e608d5cefa6546aa6ddfd33697e3968b4c3cb8
Devin Lim <davay@devinl.im>
2025-03-27 10:18:40 -0700

    checkpoint: web sleep 2 sec before lsof

--- web/start.sh
+++ web/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 nohup pnpm run dev > log 2>&1 &
-sleep 1
+sleep 2
 (lsof -i :5173 | awk 'NR>1 {print $2}' | head -n 1) > pid
 echo "Web server running with PID $(cat pid). Logging in 'log'."
