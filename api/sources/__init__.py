8af86be8309d7c6280c5d90cfd4192b03e4b217d
Devin Lim <davay@devinl.im>
2025-04-19 17:27:38 -0700

    feat: amd, boston dynamics, google, hudson river trading, huggingface, jane st, mistral, neuralink, openai, snowflake

--- api/sources/__init__.py
+++ api/sources/__init__.py
@@ -1,15 +1,35 @@
 from .amazon import amazon_sources
+from .amd import amd_sources
 from .anthropic import anthropic_sources
 from .apple import apple_sources
+from .boston_dynamics import boston_dynamics_sources
+from .google import google_sources
+from .hudson_river_trading import hudson_river_trading_sources
+from .huggingface import huggingface_sources
+from .jane_street import jane_street_sources
 from .meta import meta_sources
 from .microsoft import microsoft_sources
+from .mistral import mistral_sources
+from .neuralink import neuralink_sources
 from .nvidia import nvidia_sources
+from .openai import openai_sources
+from .snowflake import snowflake_sources
 
 all_sources = [
     amazon_sources,
+    amd_sources,
     anthropic_sources,
     apple_sources,
-    microsoft_sources,
-    nvidia_sources,
+    boston_dynamics_sources,
+    google_sources,
+    hudson_river_trading_sources,
+    huggingface_sources,
+    jane_street_sources,
     meta_sources,
+    microsoft_sources,
+    mistral_sources,
+    neuralink_sources,
+    nvidia_sources,
+    openai_sources,
+    snowflake_sources,
 ]
